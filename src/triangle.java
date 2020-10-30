f7cc587deec0020f38b5eca7aa96723f1d344d82
JustGinger888 <ethanmoffatt888@icloud.com>
2020-10-30 21:14:04 +0000

    upd

---
+++ src/triangle.java
@@ -0,0 +1,23 @@
+import java.rmi.activation.ActivationGroup_Stub;
+
+public class triangle {
+    static int NOS = 3;
+
+    double base;
+    double height;
+    double sideLenOne;
+    double sideLenTwo;
+    double sideLenThree;
+
+    public triangle(double base, double height, double sideLenOne, double sideLenTwo, double sideLenThree) {
+        this.base = base;
+        this.height = height;
+        this.sideLenOne = sideLenOne;
+        this.sideLenTwo = sideLenTwo;
+        this.sideLenThree = sideLenThree;
+    }
+
+    public double findArea() {
+        return (this.base * this.height) / 2;
+    }
+}
---
+++ src/triangleMain.java
@@ -0,0 +1,8 @@
+public class triangleMain {
+    public static void main(String[] args) {
+        triangle triangleA = new triangle(15,8,8,7,6);
+
+        double triangleAArea = triangleA.findArea();
+        System.out.println(triangleAArea);
+    }
+}
