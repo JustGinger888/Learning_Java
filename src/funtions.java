7c965f6ab724f1f89816fc85427aa8ee010da48a
JustGinger888 <ethanmoffatt888@icloud.com>
2020-10-30 20:22:45 +0000

    upd

--- src/funtions.java
+++ src/funtions.java
@@ -1,2 +1,20 @@
 public class funtions {
+
+    public static double salaryCalc(double HPW, double RPH, double VD) {
+        if (HPW < 0) {
+            return -1;
+        }
+        if (RPH < 0) {
+            return -1;
+        }
+
+        double WP = HPW * RPH;
+        double UT = VD * RPH * 8;
+        return  (WP * 52) - UT;
+    }
+
+    public static void main(String[] args) {
+        double salary = salaryCalc(40, 10, 8);
+        System.out.println(salary);
+    }
 }
